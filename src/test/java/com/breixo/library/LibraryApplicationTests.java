e34f7cab6773b0910b6195f4e259a4045da00328
BreiMato <breimatom@gmail.com>
2026-03-23 19:30:31 +0100

    get user by id with tests

--- src/test/java/com/breixo/library/LibraryApplicationTests.java
+++ src/test/java/com/breixo/library/LibraryApplicationTests.java
@@ -2,6 +2,7 @@ package com.breixo.library;
 
 import org.junit.jupiter.api.Test;
 import com.breixo.library.infrastructure.adapter.output.mybatis.BookMyBatisMapper;
+import com.breixo.library.infrastructure.adapter.output.mybatis.UserMyBatisMapper;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
@@ -19,6 +20,9 @@ class LibraryApplicationTests {
 
     @MockBean
     BookMyBatisMapper bookMyBatisMapper;
+
+    @MockBean
+    UserMyBatisMapper userMyBatisMapper;
 
     @Test
     void contextLoads() {
